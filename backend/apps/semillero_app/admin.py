703979c57554eec55cb0cb86a37f12a8df1bf067
George Alvarado <georgealvarado@outlook.com>
2022-05-23 16:54:08 -0500

    project upload

---
+++ backend/apps/semillero_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Semillero, Docente
+
+# Register your models here.
+admin.site.register(Semillero)
+admin.site.register(Docente)
+# admin.site.register(idioma)
