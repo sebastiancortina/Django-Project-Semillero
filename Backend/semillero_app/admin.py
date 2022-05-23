a35cf2e305fa3a59467647d9ef151e66f8bb2633
George Alvarado <georgealvarado@outlook.com>
2022-05-23 16:53:48 -0500

    adding vscode ignore

--- Backend/semillero_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from semillero_app.models import Semillero, Docente
-
-# Register your models here.
-admin.site.register(Semillero)
-admin.site.register(Docente)
-#admin.site.register(idioma)
-
--- Backend/semillero_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SemilleroAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'semillero_app'
