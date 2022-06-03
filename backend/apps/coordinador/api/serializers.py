00df766b37e3c87a22825924caaef3791ce52dad
George Alvarado <georgealvarado@outlook.com>
2022-06-03 08:40:45 -0500

    added coordinator serializer

--- backend/apps/coordinador/api/serializers.py
+++ backend/apps/coordinador/api/serializers.py
@@ -6,7 +6,28 @@ from apps.semillero_app.apis.serializers import SemilleroSerializer
 
 
 class CoordinadorSerializer(serializers.ModelSerializer):
-    semillerolist =SemilleroSerializer(many=True, read_only=True)
-    class Meta: 
+    semillerolist = SemilleroSerializer(many=True, read_only=True)
+
+    class Meta:
         model = Coordinador
-        fields = '__all__'
+        fields = "__all__"
+
+
+class CoordinadorUserSerializer(serializers.ModelSerializer):
+    # semillerolist = SemilleroSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Coordinador
+        fields = [
+            "n_identificacion",
+            "nombre",
+            "f_nacimiento",
+            "lugar_expedicion",
+            "lugar_nacimiento",
+            "celular",
+            "email",
+            "direcccion",
+            "c_emergencia",
+            "n_emergencia",
+            "create_at",
+        ]
